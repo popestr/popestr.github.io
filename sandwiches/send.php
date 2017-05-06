6187ea96fa5712f3c64886b3c25c1c6baa4ee3b8
Ryan Pope <2018rpope@tjhsst.edu>
2017-05-06 19:21:15 +0000

    Hid PHP

---
+++ sandwiches/send.php
@@ -0,0 +1,14 @@
+<?php
+$d = json_decode($_POST["cart"]);
+$ret = "<table><tr><th>Item</th><th>Price</th><th>Special Requests</th></tr>";
+foreach($d as $i){
+    $n = $i->n;
+    $p = $i->p;
+    $r = $i->r;
+    $ret.="<tr><td>".$n."</td><td>$".number_format((float)$p, 2, '.', '')."</td><td>".$r."</td></tr>";
+}
+$ret.="</table>";
+echo $ret;
+
+
+?>
