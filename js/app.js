aeaae8692e08b7f98626dd294cc2c9c33914dcfa
Ryan Pope <rcpope7@gmail.com>
2022-02-06 19:48:08 -0500

    cleanup js, functionally same

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
+/*
+    app.js
+    Authored by Ryan Pope <ryan@rcpope.net>
+    Licensed under the GNU Public License, v3.0
+*/
+
+// change a CSS property, for a set of elements, for a specified period of time
 function temporaryStyle(selector, property, value, delay) {
     let temp = $(selector).css(property);
     $(selector).css(property, value);
--- js/courses.js
+++ js/courses.js
@@ -1,4 +1,11 @@
-function linkFilterItems(courseSelector) {
+/*
+    courses.js
+    Authored by Ryan Pope <ryan@rcpope.net>
+    Licensed under the GNU Public License, v3.0
+*/
+
+// set up event listeners for courses
+function initCourseEvents(courseSelector) {
     $(courseSelector).each(function () {
         $(this).mouseenter(function () {
             let classes = $(this).attr("data-classifications") || ""; // data-classifications is set by php ajax request handler
@@ -13,17 +20,20 @@ function linkFilterItems(courseSelector) {
     });
 }
 
-function linkCourses(filterSelector) {
+// set up event listeners for filters
+function initFilterEvents(filterSelector) {
     $(filterSelector).each(function () {
         let cl = $(this).attr("data-classification"); // data-classification is set by php ajax request handler
+
+        // filter hover highlights courses
         $(this).mouseenter(function () {
             $(".course--" + cl).addClass("course-active");
         });
-
         $(this).mouseleave(function () {
             $(".course").removeClass("course-active");
         });
 
+        // filter select
         $(this).click(function () {
 
             // This function is intended to track the number of filters selected, as well as the number of filter matches for each course.
@@ -34,17 +44,24 @@ function linkCourses(filterSelector) {
             // In OR mode, a course must have at least 1 filter match to be shown, 
             // increasing the total number of matches with each added filter.
 
+            // check if we are selecting the filter or deselecting it by checking the existing classes
             const delta = $(this).hasClass("filter-selected") ? -1 : 1;
             $(this).toggleClass("filter-selected");
+
+            // increment result count for filter container (total number of filters selected)
             $(this).parent().attr("data-selected", parseInt($(this).parent().attr("data-selected")) + delta);
 
             $(".course--" + cl).each(function () {
+
+                // increment match count (stored in data-selectedby) for each course
                 let sb = parseInt($(this).attr("data-selectedby")) + delta;
                 $(this).attr("data-selectedby", sb);
 
+                // increment result count (stored in data-contains) for parent (.course-group)
                 let pc = parseInt($(this).parent().attr("data-contains")) + delta;
                 $(this).parent().attr("data-contains", pc);
 
+                // increment result count (stored in data-contains) for results container (total number of results)
                 let mc = parseInt($("#main-course-list").attr("data-contains")) + delta;
                 $("#main-course-list").attr("data-contains", mc);
             });
@@ -57,86 +74,91 @@ function toggleCourseGroup(groupHeader) {
     $(groupHeader).parent().toggleClass("course-group--collapsed");
 }
 
-$.getJSON("https://api.rcpope.net/courses_json.php", function (data) {
-    let last_sem = "Fall 1969";
-    let courseOut = "";
-    let languagesOut = "";
-    let classificationOut = "";
-    let group = -1;
+window.onload = function () {
+    $.getJSON("https://api.rcpope.net/courses_json.php", function (data) {
+        let last_sem = "Fall 1969";
+        let courseOut = "";
+        let languagesOut = "";
+        let classificationOut = "";
+        let group = -1;
 
-    // This section is a little janky, and I'm okay with that for now.
-    // Ideally, phase two of this redesign will involve a reactive framework, 
-    // making all this DOM interpolation wizardry wholly unnecessary.
-    for (const course of data.courses) {
-        let group_header = "";
+        // This section is a little janky, and I'm okay with that for now.
+        // Ideally, phase two of this redesign will involve a reactive framework, 
+        // making all this DOM interpolation wizardry wholly unnecessary.
+        for (const course of data.courses) {
+            let group_header = "";
 
-        if (course.semester != last_sem) {
-            group++;
-            group_header = `<div class="course-group-header" onclick="toggleCourseGroup(this)"><i class="fas fa-caret-down course-group-arrow"></i> ${course.semester}</div>`;
-            last_sem = course.semester;
-            $(".courses-list").append(`
-                <div class="course-group" data-contains="0" data-group="${group}" >
+            if (course.semester != last_sem) {
+                group++;
+                group_header = `<div class="course-group-header" onclick="toggleCourseGroup(this)"><i class="fas fa-caret-down course-group-arrow"></i> ${course.semester}</div>`;
+                last_sem = course.semester;
+                $(".courses-list").append(`
+                    <div class="course-group" data-contains="0" data-group="${group}" >
+                    </div>
+                `)
+            }
+
+            $(`.course-group[data-group="${group}"]`).append(`
+
+                ${group_header}
+                <div class="course ${course.course_classes}" data-classifications="${course.classification} ${course.languages}" data-selectedby="0">
+                    <span class="course-title">
+                        <span class="course-code">${course.course_code}</span>
+                        <span class="course-name">${course.course_topic || course.course_name}</span>
+                    </span>
+                    <span class="course-icons">
+                        <span class="course-classifications">
+                            ${course.classification_icons || ""}
+                        </span>
+                        <span class="course-languages">
+                            ${course.lang_icons || ""}
+                        </span>
+                    </span>
                 </div>
-            `)
+
+            `);
         }
 
-        courseOut = `
-        ${group_header}
-        <div class="course ${course.course_classes}" data-classifications="${course.classification} ${course.languages}" data-selectedby="0">
-            <span class="course-title">
-                <span class="course-code">${course.course_code}</span>
-                <span class="course-name">${course.course_topic || course.course_name}</span>
-            </span>
-            <span class="course-icons">
-                <span class="course-classifications">
-                    ${course.classification_icons || ""}
-                </span>
-                <span class="course-languages">
-                    ${course.lang_icons || ""}
-                </span>
-            </span>
-        </div>
-        `;
+        for (const c of data.classifications) {
+            $(".filter-group--class").append(`
 
-        $(`.course-group[data-group="${group}"]`).append(courseOut);
-    }
+                <div class="filter-item filter-item--${c.abbreviation}" data-classification="${c.abbreviation}">
+                    <span class="icon-wrapper">${c.icon_html}</span>
+                    <span class="filter-text">
+                        ${c.longname}
+                    </span>
+                </div>
 
-    for (const c of data.classifications) {
-        classificationOut += `
-        <div class="filter-item filter-item--${c.abbreviation}" data-classification="${c.abbreviation}">
-            <span class="icon-wrapper">${c.icon_html}</span>
-            <span class="filter-text">
-                ${c.longname}
-            </span>
-        </div>
-        `;
-    }
+            `);
+        }
 
-    $(".filter-group--class").append(classificationOut);
+        for (const l of data.languages) {
+            $(".filter-group--lang").append(`
 
-    for (const l of data.languages) {
-        languagesOut += `
-        <div class="filter-item filter-item--${l.abbreviation}" data-classification="${l.abbreviation}">
-            <span class="icon-wrapper">${l.icon_html}</span>
-            <span class="filter-text">
-                ${l.longname}
-            </span>
-        </div>
-        `;
-    }
+                <div class="filter-item filter-item--${l.abbreviation}" data-classification="${l.abbreviation}">
+                    <span class="icon-wrapper">${l.icon_html}</span>
+                    <span class="filter-text">
+                        ${l.longname}
+                    </span>
+                </div>
+                
+            `);
+        }
 
-    $(".filter-group--lang").append(languagesOut);
+        initCourseEvents(".course");
+        initFilterEvents(".filter-item");
 
-    linkFilterItems(".course"); // make hovering over a course highlight appropriate legend entries
-    linkCourses(".filter-item"); // make overing over a legend entry highlight appropriate courses
+        // select a couple filters so the results aren't empty on pageload
+        $(".filter-item--imp").click();
+        $(".filter-item--theo").click();
 
-    $(".filter-item--imp").click();
-    $(".filter-item--theo").click();
+        // unhide the filters and results
+        $(".filter-list").css("display", "flex");
+        $("#main-course-list").attr("data-loading", "false");
 
-    $(".filter-list").css("display", "flex");
-    $("#main-course-list").attr("data-loading", "false");
-
-    $(".loading-icon-container").css('display', 'none');
+        // hide the loading icons
+        $(".loading-icon-container").css('display', 'none');
 
 
-});
+    });
+}
